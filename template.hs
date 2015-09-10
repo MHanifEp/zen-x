42c6b78a687f5d70ad77ef61db3450488b4ff253
MHanifEp <mhanif.ep@gmail.com>
2015-09-10 20:47:33 +0700

    nganu
    nganu anu @squest not done tho

--- template.hs
+++ template.hs
@@ -16,3 +16,235 @@ take 'x = x
 
 
 -- ndewe
+
+take' x = x
+
+--nganu anuan
+
+drop' x = x
+
+--nganu anuan
+
+fst' x = x
+
+--nganu anuan
+
+snd' x = x
+
+--nganu anuan
+
+map' x = x
+
+--nganu anuan
+
+filter' x = x
+
+--nganu anuan
+
+delete' x = x
+
+--nganu anuan
+
+deleteAll' x = x
+
+--nganu anuan
+
+foldl' x = x
+
+--nganu anuan
+
+foldl1' x = x
+
+--nganu anuan
+
+zip' x = x
+
+--nganu anuan
+
+zipWith' x = x
+
+--nganu anuan
+
+nth' x = x
+
+--nganu anuan
+
+scanl' x = x
+
+--nganu anuan
+
+scanl1' x = x
+
+--nganu anuan
+
+elem' x = x
+
+--nganu anuan
+
+notElem' x = x
+
+--nganu anuan
+
+head' x = x
+
+--nganu anuan
+
+length' x = x
+
+--nganu anuan
+
+reverse' x = x
+
+--nganu anuan
+
+last' x = x
+
+--nganu anuan
+
+tail' x = x
+
+--nganu anuan
+
+init' x = x
+
+--nganu anuan
+
+max' x = x
+
+--nganu anuan
+
+min' x = x
+
+--nganu anuan
+
+concat' x = x
+
+--nganu anuan
+
+intersperse' x = x
+
+--nganu anuan
+
+intercalate' x = x
+
+--nganu anuan
+
+and' x = x
+
+--nganu anuan
+
+or' x = x
+
+--nganu anuan
+
+zip3' x = x
+
+--nganu anuan
+
+sum' x = x
+
+--nganu anuan
+
+product' x = x
+
+--nganu anuan
+
+words' x = x
+
+--nganu anuan
+
+lines' x = x
+
+--nganu anuan
+
+unlines' x = x
+
+--nganu anuan
+
+unwords' x = x
+
+--nganu anuan
+
+takeWhile' x = x
+
+--nganu anuan
+
+dropWhile' x = x
+
+--nganu anuan
+
+concatMap' x = x
+
+--nganu anuan
+
+all' x = x
+
+--nganu anuan
+
+any' x = x
+
+--nganu anuan
+
+insert' x = x
+
+--nganu anuan
+
+zipWith3' x = x
+
+--nganu anuan
+
+
+
+
+
+-- nganu.b
+nub' x = x
+
+--nganu anuan
+
+sort' x = x
+
+--nganu anuan
+
+minimum' x = x
+
+--nganu anuan
+
+maximum' x = x
+
+--nganu anuan
+
+inits' x = x
+
+--nganu anuan
+
+tails' x = x
+
+--nganu anuan
+
+union' x = x
+
+--nganu anuan
+
+intersect' x = x
+
+--nganu anuan
+
+group' x = x
+
+--nganu anuan
+
+splitAt' x = x
+
+--nganu anuan
+
+partition' x = x
+
+--nganu anuan
+
+replicate' x = x
+
+--nganu anuan
+
+-- anu nganu anu /?
+--haven't finished yets (ofc dude)
